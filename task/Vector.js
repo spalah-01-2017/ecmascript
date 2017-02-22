eb0d89b89a2b65a77d626bade0402cc660b07c43
Denis Golovin <denis_golovin@mail.ua>
2017-02-22 18:16:26 +0200

    1st task

---
+++ task/Vector.js
@@ -0,0 +1,20 @@
+function Vector (x, y) {
+	this.x = x;
+	this.y = y;
+	return this;
+}
+Vector.prototype.plus = function(vector) {
+	this.x += vector.x;
+	this.y += vector.y;
+	return this;
+}
+Vector.prototype.minus = function(vector) {
+	this.x -= vector.x;
+	this.y -= vector.y;
+	return this;
+}
+Object.defineProperty(Vector.prototype, "length", {
+  get: function() {
+	return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+}
+});
