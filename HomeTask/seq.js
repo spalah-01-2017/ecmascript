acecf1462f0f0e8b0a70cd6d7b5014b148253a8e
Aleksey Kuzmenko <kurtzpb@gmail.com>
2017-02-23 21:54:10 +0200

    Home Work

--- HomeTask/seq.js
+++ HomeTask/seq.js
@@ -1,15 +1,15 @@
-function Seq (arr = []) {
+function Seq (...arr) {
 	let counter = 0;
-	arr.forEach((el, ind) => this[ind] = el, this);
+	if (arr.length === 1 && arr[0].length) arr = arr[0];
+	arr.forEach((el, ind) => this[ind] = el);
 }
 
-Seq.prototype.get = function (...arg) {
-	if (typeof arg[0] === 'object') arg = arg[0];
-	return arg.map(el => +el in this ? this[el] : null, this);
+Seq.prototype.get = function (arg) {
+	return arg in this ? this[arg] : null;
 }
 
 Seq.prototype.has = function (arg) {
-	return Object.keys(this).some(el => this[el] === +arg, this);
+	return Object.keys(this).some(el => this[el] === arg);
 }
 
 Seq.prototype.next = function () {
@@ -18,5 +18,9 @@ Seq.prototype.next = function () {
 }
 
 Seq.prototype.toArray = function () {
-	return Object.keys(this).map(el => this[el], this);
+	return Object.keys(this).map(el => this[el]);
 }
+
+let s = new Seq([[1, 3, 4], 3, 4, 'a', 6])
+
+console.log(s.has(6));
