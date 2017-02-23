09147ae198290a0dc40146f61742a7c811bb8671
Mike Parkhomenko <mikye777@mail.ru>
2017-02-23 19:08:47 +0200

    My Homework

---
+++ task/Vector.js
@@ -0,0 +1,35 @@
+function Vector(x, y) {
+	this.x = x;
+  this.y = y;
+  
+  this.plus = function(vector) {
+  	newX = this.x + vector.x;
+    newY = this.y + vector.y;
+    return newVec = {
+    	x: newX,
+      y: newY
+    };
+  }; 
+  
+ 	this.minus = function(vector) {
+  	newX = this.x - vector.x;
+    newY = this.y - vector.y;
+    return newVec = {
+    	x: newX,
+      y: newY
+    };
+  };
+  
+  Object.defineProperty(this, "length", {
+  	get: function() {
+  		return Math.sqrt( Math.pow(this.x,2) + Math.pow(this.y,2) );
+    }
+  });
+}
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length);
+// → 5
